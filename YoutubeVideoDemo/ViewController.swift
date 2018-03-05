e3351a8c1e26ad3f146852375c3ee9b42cb1fb2b
Kondiram Sonawane <Kondya@Kondirams-MacBook-Air.local>
2018-03-05 21:43:17 +0530

    youtue

--- YoutubeVideoDemo/ViewController.swift
+++ YoutubeVideoDemo/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+
+    @IBOutlet weak var webView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        webView.allowsInlineMediaPlayback = true
+        webView.mediaPlaybackRequiresUserAction = false
+        let videoID = "SfcpAV-8qtU"
+        
+        // Set up your HTML.  The key URL parameters here are playsinline=1 and autoplay=1
+        // Replace the height and width of the player here to match your UIWebView's  frame rect
+        
+        
+        
+        let embededHTML = "<html><body style='margin:0px;padding:0px;'><script type='text/javascript' src='http://www.youtube.com/iframe_api'></script><script type='text/javascript'>function onYouTubeIframeAPIReady(){ytplayer=new YT.Player('playerId',{events:{onReady:onPlayerReady}})}function onPlayerReady(a){a.target.playVideo();}</script><iframe id='playerId' type='text/html' width='\(self.webView.frame.size.width+50)' height='\(self.webView.frame.size.height)' src='http://www.youtube.com/embed/\(videoID)?enablejsapi=1&rel=0&controls=1&showinfo=0&playsinline=1&autoplay=0' frameborder='0'></body></html>"
+        
+       
+        webView.loadHTMLString(embededHTML, baseURL: Bundle.main.bundleURL)
+        
+       
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+   
 
 }
 
